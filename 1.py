9ea162b5923895adc204edcca692ae4f52b2099f
Евгений <baidak0310@gmail.com>
2024-12-07 10:19:30 +0200

    dzz2

---
+++ 1.py
@@ -0,0 +1,12 @@
+print("to be\n\n\tor not\n\n\t\tto be...")
+print("\n\t\t\t\\ william shakespeare \\")
+print("\tДЕКАБРЬ 2024")
+print("пн вт ср чт пт сб вс")
+print("                   1")
+print(" 2  3  4  5  6  7  8")
+print(" 9 10 11 12 13 14 15")
+print("16 17 18 19 20 21 22")
+print("23 24 25 26 27 28 29")
+print("30 31")
+print
+print
